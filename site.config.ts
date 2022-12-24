f6735b3b854a69ee011ee48086484f6a50d28510
qingyan <49362778+kenyon01@users.noreply.github.com>
2022-12-24 16:40:06 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'Reading-List-6e0ed4247d4b4453bbb9dafea9009c43',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Notion site',
+  domain: 'https://kenyon01.notion.site/Reading-List-6e0ed4247d4b4453bbb9dafea9009c43',
+  author: 'qingyan',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
