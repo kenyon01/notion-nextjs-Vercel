fa40dca99504aa096c0e740827b8eaed2529abd4
qingyan <49362778+kenyon01@users.noreply.github.com>
2022-12-25 19:36:36 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'Reading-List-6e0ed4247d4b4453bbb9dafea9009c43',
+  rootNotionPageId: 'Qingyan-s-HOME-4abcbd36a562452abd991abc2af4e931',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,16 +10,16 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Notion site',
-  domain: 'https://kenyon01.notion.site/Reading-List-6e0ed4247d4b4453bbb9dafea9009c43',
+  domain: 'https://kenyon01.notion.site/Qingyan-s-HOME-4abcbd36a562452abd991abc2af4e931',
   author: 'qingyan',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  //twitter: 'transitive_bs',
+  github: 'https://github.com/kenyon01/',
+  //linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
